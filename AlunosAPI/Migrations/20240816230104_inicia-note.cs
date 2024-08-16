7fabdd28d9373e79ceadec179c57f0b2396e4da3
Vinicius Estevo <viniestevo33@gmail.com>
2024-08-16 20:21:31 -0300

    Add project files.

---
+++ AlunosAPI/Migrations/20240816230104_inicia-note.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AlunosAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicianote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "mariapenha@yahoo.com", 23, "Maria da Penha" },
+                    { 2, "manuelbueno@yahoo.com", 22, "Manuel Bueno" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alunos");
+        }
+    }
+}
---
+++ AlunosAPI/Services/IAlunoService.cs
@@ -0,0 +1,14 @@
+﻿using AlunosAPI.Models;
+
+namespace AlunosAPI.Services
+{
+    public interface IAlunoService
+    {
+        Task<IEnumerable<Aluno>> GetAlunos();
+        Task<Aluno> GetAluno(int id);
+        Task<IEnumerable<Aluno>> GetAlunosByName(string nome);
+        Task CreateAluno(Aluno aluno);
+        Task UpdateAluno(Aluno aluno);
+        Task DeleteAluno(Aluno aluno);
+    }
+}
